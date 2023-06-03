19b03c5b19f9a191f96e143cbdc4552f64fe5536
Daniel Reagan <d.reagan.dr@gmail.com>
2023-06-03 16:38:37 -0500

    Add Resume button functionality

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import photo from '../images/profile-photo.jpg';
+import resume from '../files/TempResume.pdf'
 
 export default function Hero({contactFormRef}) {
     return (
         <main>
             <div className="hero-grid">
                 <h1 className="span-2">Info about me...</h1>
-                <button>Get Resume</button>
+                <a href={resume} download="Resume"><button>Get Resume</button></a>
                 <button 
                     onClick={() => contactFormRef.current.scrollIntoView({behavior: 'smooth'})}
                 >Contact Me</button>
